ca7cca6a2ba578767a83fb5f9ac31adfc1621fa8
toyto <anko@unisport.dk>
2020-08-05 17:34:04 +0300

    Added few indexes

--- init.sql
+++ init.sql
@@ -6,5 +6,9 @@ CREATE TABLE fields_field ("id" serial NOT NULL PRIMARY KEY,"crop" varchar(255),
 SELECT AddGeometryColumn ('fields_field', 'geom', 4326, 'MultiPolygon', 2);
 SELECT AddGeometryColumn ('fields_field', 'geom_p', 4326, 'Polygon', 2);
 
+CREATE INDEX "field_crop" on "fields_field" ("crop");
+CREATE INDEX "field_region" on "fields_field" ("region");
+CREATE INDEX "field_geom" on "fields_field" USING GIST ("geom");
+
 # Just test queries. No need to run them.
 SELECT id, ST_AsGeoJSON(geom) FROM fields_field WHERE ST_Intersects(geom::geography, ST_Buffer(ST_MakePoint(-3.0282305, 48.3033958)::geography, 5000, 'quad_segs=8')) IS True;
